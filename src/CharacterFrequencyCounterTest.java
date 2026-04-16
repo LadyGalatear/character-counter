04680cb4392c76b8da7d7698db8e8e6a9348d9e2
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-04-16 11:10:56 -0700

    Fix getRelativePercentage method, write tests

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,27 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    void testCharacterShowsUpMultipleTimesAndCountsCorrectly() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        // Act
+        int actual = counter.getFrequency('l');
+
+        // Assert
+        assertEquals(2, actual);
+    }
+
+    @Test
+    void testGetRelativeFrequencyReturns0Correctly() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        // Act
+        double actual = counter.getRelativePercentage('x');
+
+        //Assert
+        assertEquals(0.00, actual, 0.0001);
+    }
 }
